554decdc8cdef10ad7252dfe599e24e6cd4e9ee0
Andrew Megaris <a711m819@ku.edu>
2016-08-24 14:47:40 -0500

    closer

--- linkedList.h
+++ linkedList.h
@@ -1,24 +1,36 @@
-/////////////////////////////////////////////////////////////////////////
-// linkedList.h
-// eecs560_lab1
-// Andrew Megaris
-/////////////////////////////////////////////////////////////////////////
+/*///////////////////////////////////////////////////////////////////////
+//--linkedList.h
+//-- eecs560_lab1
+//--Andrew Megaris
+//////////////////////////////////////////////////////////////////////*/
+
 #ifndef _LINKEDLIST
 #define _LINKEDLIST
 
 
 //local include
-#include "linkedList.cpp"
-#include "node.h"
-class linkedList{
-public:
-    linkedList();
-    ~linkedList();
+//#include "linkedList.cpp"
 
+//Node Structure Definition
+struct Node{
+    int value;
+    Node *nodePtr;
+};
+
+//Linked List Class Definition
+class LinkedList{
+public:
+    //Constructor and Deconstructor
+    LinkedList();
+    ~LinkedList();
+    //Pointer for start of list
+    Node *headPtr = NULL;
+
+    //Linked List Function Definitions
     void print();
     void insert(int);
-
-private:
-    node* headNode;
+    bool isEmpty();
+    void traverseList(bool,bool,int);
 };
+
 #endif
--- main.cpp
+++ main.cpp
@@ -1,59 +1,19 @@
-#include <iostream>
 #include <fstream>
 #include <string>
+#include "linkedList.cpp"
 
 //main
-int main(){
-    std::string input;
-    std::ifstream myfile;
-    myfile.open ("data.txt");
-    if(myfile.is_open()){
-        while (getline (myfile,input))
-        {std::cout << input << '\n';}
-        myfile.close();
+int main(int argc, char* argv[]){
+    LinkedList *list;
+ //   list = new LinkedList();
 
-    }
-    else
-        std::cout << "Error opening file";
+    //print empty list test
+    list -> print();
 
+    list -> insert(10);
+    list -> print();
+
+    list -> insert(20);
+    list -> print();
     return 0;
-}/*
-	node *root;
-	node *nextNode;
-	
-	root = new node;
-	root -> next = 0;
-	root -> value = 1;
-
 }
-
-//print function.
-void print(){
-	nextNode =  root;//Set nextNode to top of list.
-	//check for empty list
-	if (nextNode != 0){
-		while (nextNode -> next != 0)
-			std::cout << nextNode-> value; //output current value
-			nextNode = nextNode -> next;	 //move to next node
-	}
-}
-
-//checks if input value is currently in list.
-//if not inserts value to the end of the list.  
-void insert(x){
-	//reset nextNode 
-	nextNode = root;
-	
-	//check for empty list and move to the end.
-	if( nextNode != 0 ){
-		while(nextNode -> next != 0){
-		nextNode = nextNode -> next;	
-		}
-	}
-	nextNode -> next = new node; //add a new node at the end of the list
-	nextNode = nextNode -> next; //advance to new last node
-	nextNode -> next = 0; //cap new last node
-	nextNode -> value = x; //assign new input value
-	
-}
-*/
